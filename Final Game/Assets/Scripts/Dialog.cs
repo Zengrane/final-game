89f5da7b91c388921c9b36048a5002a70c24e7dc
Zengrane <samuelsantucci04@gmail.com>
2021-09-21 21:27:12 +0930

    Dialogue Updated
    Improved dialogue

--- Final Game/Assets/Scripts/Dialog.cs
+++ Final Game/Assets/Scripts/Dialog.cs
@@ -5,29 +5,53 @@ using TMPro;
 
 public class Dialog : MonoBehaviour
 {
+    public GameObject textBox; // Gets the text box game object
     public TextMeshProUGUI textDisplay;
     public string[] sentences; // Holds all sentences that make up dialog
     private int index;
     public float typingSpeed;
-
     public GameObject continueButton;
     
-
     void Start()
     {
-        StartCoroutine(Type());
-    }
+        continueButton.SetActive(false);
+        textBox.SetActive(false);
 
+       
+        
+    }
     void Update()
     {
-        if(textDisplay.text == sentences[index]) 
+        if (textDisplay.text == sentences[index])
         {
             continueButton.SetActive(true);
+            textBox.SetActive(true);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Player in range");
+           
+                StartCoroutine(Type());
+            
+            
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Player left range");
         }
     }
 
     IEnumerator Type() //Couroutine allows to execute game logic over a number of frames
     {
+
         foreach (char letter in sentences[index].ToCharArray())
         {
             textDisplay.text += letter;
@@ -51,6 +75,9 @@ public class Dialog : MonoBehaviour
         {
             textDisplay.text = "";
             continueButton.SetActive(false);
+            textBox.SetActive(false);
+
+
         }
     }
    
